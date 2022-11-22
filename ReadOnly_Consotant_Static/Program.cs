9920a6f1003e1b0debe43e5c1a88881fd3fc09ed
Faruk ul Islam <faruk.ice.pust@gmail.com>
2022-11-22 06:25:48 +0600

    FirstUpLoad

---
+++ ReadOnly_Consotant_Static/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadOnly_Consotant_Static
+{
+    class Program
+    {
+        readonly int Readonly;
+        const int Constant = 10;
+        static int Static=5;
+        int Integer = 15;
+        public static void Example(){
+            //Constant++;
+         //  Static= 5;
+            Static++;
+            Console.WriteLine(Static);
+        }
+        static void Main(string[] args)
+        {
+           Example();
+           Example();
+           Example();
+            
+            Console.ReadKey();
+        }
+    }
+}
